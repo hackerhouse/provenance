028a2e6aa7acc486c1384a9a10246e9d865892eb
zv <zv@nxvr.org>
2014-02-28 23:50:51 -0800

    Fixed caller error, bound function invocation rather than computed.

--- app/js/background.js
+++ app/js/background.js
@@ -1,20 +1,19 @@
-
 var coll = new Collector();
 var _tab; // tracks state of content.js's tab & ctx
 
 /*
-  Attempt to refresh/sync latest _tab data on tab activity
-*/
+   Attempt to refresh/sync latest _tab data on tab activity
+   */
 chrome.tabs.onActivated.addListener(function(t){
-    chrome.tabs.get(t.tabId, function(tab) {
-	if (tab && tab.url !== 'chrome://history/') {
-	    _tab = tab;
-	}
-    });
+  chrome.tabs.get(t.tabId, function(tab) {
+    if (tab && tab.url !== 'chrome://history/') {
+      _tab = tab;
+    }
+  });
 });
 
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request) {
-	console.log('received request');
-    }
+  if (request) {
+    console.log('received request');
+  }
 });
