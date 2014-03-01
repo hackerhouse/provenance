b0bbd92628fee0eb90cd3d7c4577c3b8388f4f34
zv <zv@nxvr.org>
2014-02-28 23:03:12 -0800

    Added jsdoc specifiers

--- app/js/collector.js
+++ app/js/collector.js
@@ -18,8 +18,9 @@ function Collector() {
 
 Collector.prototype = {
 
-  /*
-   * send a message containing our completed URLs
+  /**
+   * Persists our state to the storage API.
+   * @private
    */
   saveDataState: function() {
     chrome.runtime.sendMessage({
@@ -31,13 +32,13 @@ Collector.prototype = {
   },
 
   /**
-   * Creates an empty entry in the pending array if one doesn't already exist,
-   * and prepopulates the errored and completed arrays for ease of insertion
-   * later.
-   *
-   * @param {!string} id The request's ID, as produced by parseId_.
-   * @param {!string} url The request's URL.
-   */
+    Creates an empty entry in the pending array if one doesn't already exist,
+    and prepopulates the errored and completed arrays for ease of insertion
+    later.
+
+    @param {!string} id The request's ID, as produced by parseId_.
+    @param {!string} url The request's URL.
+   **/
   prepareUrlStorage: function(id, url) {
     this._pending = this._pending[id] || {
       source: {
@@ -52,9 +53,16 @@ Collector.prototype = {
     }
   }
 
-  /*
-   * Handler for the onCreatedNavigationTarget event. Updates a pending request
-   * in the source tab, and notes that it opened a new tab.
+  /**
+   * Handler for the 'onCreatedNavigationTarget' event. Updates the
+   * pending request with a source frame/tab, and notes that it was opened in a
+   * new tab.
+   *
+   * Pushes the request onto the
+   * 'pending_' object, and stores it for later use.
+   *
+   * @param {!Object} data The event data generated for this request.
+   * @private
    */
   createdNavigationTargetListener: function(data) {
     var tabid = this.parseId(data);
@@ -67,9 +75,12 @@ Collector.prototype = {
     this.pending_[tabid].timestamp = data.timeStamp;
   },
 
-  /*
+  /**
    * Handler for the 'onBeforeNavigate' event. Pushes the request onto the
    * 'pending_' object, and stores it for later use.
+   *
+   * @param {!Object} data The event data generated for this request.
+   * @private
    */
   onBeforeNavigateListener: function(data) {
     var tabid = this.parseId(data);
@@ -78,9 +89,15 @@ Collector.prototype = {
   },
 
   /**
-    Handler for the 'onCommitted' event. Updates the pending request with
-    transition information.
-  */
+   * Handler for the 'onCommitted' event. Updates the pending request with
+   * transition information.
+   *
+   * Pushes the request onto the
+   * 'pending_' object, and stores it for later use.
+   *
+   * @param {!Object} data The event data generated for this request.
+   * @private
+   */
   onCommittedListener: function(data) {
     var id = this.parseId(data);
     this.prepareUrlStorage(id, data.url);
@@ -89,8 +106,14 @@ Collector.prototype = {
   },
 
 
-  /* Handles a web page's completion event, grabs page out of
-   * oncommitedlistener's pending queue */
+  /**
+   * Handler for the 'onCompleted` event. Pulls the request's data from the
+   * 'pending_' object, combines it with the completed event's data, and pushes
+   * a new NavigationCollector.Request object onto 'completed_'.
+   *
+   * @param {!Object} data The event data generated for this request.
+   * @private
+   */
   onCompleted: function(data) {
     var id = this.parseId(data);
     this._completed[data.url].push({
@@ -106,7 +129,9 @@ Collector.prototype = {
   },
 
 
-  // tabId ffunction from webnavigation API example.
+  /**
+    tabId ffunction from webnavigation API example.
+    **/
   parseId: function(data) {
     return data.tabId + '-' + (data.frameId ? data.frameId : 0);
   }
