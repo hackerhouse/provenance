7cee39424853f4722d676c98ae674a840680eb88
Michael E. Karpeles <michael.karpeles@gmail.com>
2014-02-28 23:21:17 -0800

    adding history, removing popups and utils

---
+++ app/js/history.js
@@ -0,0 +1,24 @@
+
+function render_history(tab) { 
+    $('#container').html(JSON.stringify(tab));
+}
+
+/* Triggered when the chrome://history page is visited */
+document.addEventListener('DOMContentLoaded', function () {
+    if (chrome.extension.getBackgroundPage()._tab) {
+	// Sync / update ctx from content.js via background.js
+	tab = chrome.extension.getBackgroundPage()._tab;
+	render_history(tab);
+    }
+});
+
+/*
+  Listens for requests from background.js and replies with a msg
+  containing url and html
+*/
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    var msg = {url: window.location.href,
+	       html: document.body.innerHTML
+	      }
+    sendResponse(msg);
+});
