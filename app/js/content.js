93e0de188befd5d805d7f13604d7c33ba17d9575
Michael E. Karpeles <michael.karpeles@gmail.com>
2014-03-02 00:00:27 -0800

    adding back content script to capture page ctx/html

--- app/js/content.js
+++ app/js/content.js
@@ -9,11 +9,11 @@
   as a payload. A callback may be provided as an option argument.
 */
 function notify_bg_to(method, data, callback) {
-    cb = (callback !== undefined) ? callback : function(response) { };
-    chrome.extension.sendMessage({
-	'method': method,
-	'data': data,
-    }, cb);
+  cb = (callback !== undefined) ? callback : function(response) { };
+  chrome.extension.sendMessage({
+    'method': method,
+    'data': data,
+  }, cb);
 }
 
 /*
@@ -21,29 +21,24 @@ function notify_bg_to(method, data, callback) {
   containing url and html
 */
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    var msg = {url: window.location.href,
-	       html: document.body.innerHTML
-	      }
-    sendResponse(msg);
+  var msg = {
+    url: window.location.href,
+    html: document.body.innerHTML
+  }
+  sendResponse(msg);
 });
 
 /*
   capture() compiles and submits information about the browser's
-  current context (tab, resource, etc). Each capture represents a node
-  within a session of accessed resources. The edges between each node
-  represents an action through which the content was discovered.
+  current context.
  */
 (function capture() {
-    var ctx = {
-	url: window.location.href,
-	html: document.body.innerHTML
-    };
+  var ctx = {
+    url: window.location.href,
+    html: document.body.innerHTML
+  };
 
-    // share ctx with background.js
-    notify_bg_to('record', ctx);
-
-    // submit ctx to the server for insertion
-    $insert_node(ctx, function(response) {
-        console.log("[INSERTED]" + ctx.url);
-    });
+  // share ctx with background.js
+  notify_bg_to('record', ctx);
+  console.log('page hit');
 })();
