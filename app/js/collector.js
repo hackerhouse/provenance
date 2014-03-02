f64993112d50ab1873b8dd5f7e586d1c9a48e9db
zv <zv@nxvr.org>
2014-03-01 20:34:55 -0800

    Not using jsdoc, fuck this duck

--- app/js/collector.js
+++ app/js/collector.js
@@ -17,7 +17,6 @@ Collector.prototype = {
 
   /**
    * Persists our state to the storage API.
-   * @private
    */
   saveDataState: function() {
     chrome.runtime.sendMessage({
@@ -32,9 +31,6 @@ Collector.prototype = {
     Creates an empty entry in the pending array if one doesn't already exist,
     and prepopulates the errored and completed arrays for ease of insertion
     later.
-
-    @param {!string} id The request's ID, as produced by parseId_.
-    @param {!string} url The request's URL.
    **/
   prepareUrlStorage: function(id, url) {
     this._pending[id] = this._pending[id] || {
@@ -55,12 +51,6 @@ Collector.prototype = {
    * Handler for the 'onCreatedNavigationTarget' event. Updates the
    * pending request with a source frame/tab, and notes that it was opened in a
    * new tab.
-   *
-   * Pushes the request onto the
-   * '_pending' object, and stores it for later use.
-   *
-   * @param {!Object} data The event data generated for this request.
-   * @private
    */
   createdNavigationTargetListener: function(data) {
     var id = this.parseId(data);
@@ -76,9 +66,6 @@ Collector.prototype = {
   /**
    * Handler for the 'onBeforeNavigate' event. Pushes the request onto the
    * '_pending' object, and stores it for later use.
-   *
-   * @param {!Object} data The event data generated for this request.
-   * @private
    */
   onBeforeNavigateListener: function(data) {
     var id = this.parseId(data);
@@ -92,9 +79,6 @@ Collector.prototype = {
    *
    * Pushes the request onto the
    * '_pending' object, and stores it for later use.
-   *
-   * @param {!Object} data The event data generated for this request.
-   * @private
    */
   onCommittedListener: function(data) {
     var id = this.parseId(data);
@@ -108,9 +92,6 @@ Collector.prototype = {
    * Handler for the 'onCompleted` event. Pulls the request's data from the
    * '_pending' object, combines it with the completed event's data, and pushes
    * a new NavigationCollector.Request object onto 'completed_'.
-   *
-   * @param {!Object} data The event data generated for this request.
-   * @private
    */
   onCompleted: function(data) {
     var id = this.parseId(data);
